4091d69cfa7695077dd0b678ee3fc51965cf0818
rranuki <124467655+rranuki@users.noreply.github.com>
2024-01-18 19:39:41 +1100

    Added updated files

--- form.js
+++ form.js
@@ -1,9 +1,27 @@
-function SendAnAlert(){
-    const NameEl = document.getElementById("hi")
-    TitleEl.style = "display:none"
-
+    document.getElementById("entry.1873864105").style = "display:none;"
+    const selectedImage = JSON.parse(localStorage.getItem('selectedImage'));
+    document.getElementById("entry.1873864105").value = selectedImage.page + " | " + selectedImage.name + " | " + selectedImage.id + " | " + selectedImage.artist;
+    
+    function SendAnAlert(){
+    document.getElementById("entry.2011975985").style = "display:none;"
+    document.getElementById("entry.1547255295").style = "display:none;"
+    document.getElementById("entry.1530722911").style = "display:none;"
+    document.getElementById("entry.1066450418").style = "display:none;"
+    document.getElementById("name").style = "display:none;"
+    document.getElementById("school").style = "display:none;"
+    document.getElementById("class").style = "display:none;"
+    document.getElementById("email").style = "display:none;"
+    document.getElementById("submitIt").style = "display:none;"
+    document.getElementById("loading").style = "display:unset;"
+    document.getElementById("goBack").style = "display:none;" 
+    // document.getElementById("loadingBox").style = "text-align: center;"
+    document.getElementById("hi").innerText = "...Processing Request..."
     setTimeout(() => {
-        window.location.href = "index.html";
-    }, 5000);
+        document.getElementById("hi").innerText = "Thank you for getting a Sticker Buddy! Your sticker should arrive in about a week!"  
+        // document.getElementById("hi").style = "padding-top: 20%"  
+        document.getElementById("spriggy").style = "display:unset;"
+        document.getElementById("loading").style = "display:none;"
+        document.getElementById("cmon").style = "display:unset;"
+    }, 3000);
     
 }
