139b56249f5a13c488b3e51dd01a48e66f8bd480
rranuki <124467655+rranuki@users.noreply.github.com>
2024-01-28 17:41:21 +1100

    added updated form js

--- form.js
+++ form.js
@@ -1,13 +1,31 @@
     document.getElementById("entry.1873864105").style = "display:none;"
-document.getElementById("entry.1547255295").style = "display:none;"
     const selectedImage = JSON.parse(localStorage.getItem('selectedImage'));
     document.getElementById("entry.1873864105").value = selectedImage.page + " | " + selectedImage.name + " | " + selectedImage.id + " | " + selectedImage.artist;
-    
-    function SendAnAlert(){
+    // console.log(document.getElementById("entry.1873864105").getAttribute(length))
 
-        if(document.getElementById("entry.2011975985").value = ""){
-            alert("fill it in or else!")
-        }else{
+    
+    function ChangeScreenDynamically(){
+    //     if ((document.getElementById("entry.2011975985").value.length < 1)||(document.getElementById("entry.1530722911").value.length < 1)||(document.getElementById("entry.1066450418").value.length < 1)) {
+    //         document.getElementById("hi").innerText = "Please fill out all the details"
+    //         document.getElementById("hi").style = "color:rgb(255, 113, 113);"
+    // } else {
+
+    if (document.getElementById("entry.2011975985").value.length == "") {
+        document.getElementById("hi").innerText = "Please fill out all the details"
+        document.getElementById("hi").style = "color:rgb(172,0,0);"
+        document.getElementById("entry.2011975985").style="border:solid .5vh rgb(172,0,0);"
+    }
+    if (document.getElementById("entry.1530722911").value.length == "") {
+      document.getElementById("hi").innerText = "Please fill out all the details"
+      document.getElementById("hi").style = "color:rgb(172,0,0);"
+      document.getElementById("entry.1530722911").style="border:solid .5vh rgb(172,0,0);"
+    } 
+    if (document.getElementById("entry.1066450418").value.length == "") {
+        document.getElementById("hi").innerText = "Please fill out all the details"
+        document.getElementById("hi").style = "color:rgb(172,0,0);"
+        document.getElementById("entry.1066450418").style="border:solid .5vh rgb(172,0,0);"
+    } else{
+
     document.getElementById("entry.2011975985").style = "display:none;"
     document.getElementById("entry.1547255295").style = "display:none;"
     document.getElementById("entry.1530722911").style = "display:none;"
@@ -21,6 +39,7 @@ document.getElementById("entry.1547255295").style = "display:none;"
     document.getElementById("goBack").style = "display:none;" 
     // document.getElementById("loadingBox").style = "text-align: center;"
     document.getElementById("hi").innerText = "...Processing Request..."
+    document.getElementById("hi").style = "color:white;"
     setTimeout(() => {
         document.getElementById("hi").innerText = "Thank you for getting a Sticker Buddy! Your sticker should arrive in about a week!"  
         // document.getElementById("hi").style = "padding-top: 20%"  
@@ -28,6 +47,7 @@ document.getElementById("entry.1547255295").style = "display:none;"
         document.getElementById("loading").style = "display:none;"
         document.getElementById("cmon").style = "display:unset;"
     }, 3000);
-        }
-        }
-
+    }
+   
+    
+}
